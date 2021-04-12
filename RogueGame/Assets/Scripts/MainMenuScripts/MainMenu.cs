d249e433806b4503482d9e6e666c9c9761e25949
huligabuliga <jonastclausen@gmail.com>
2021-04-12 17:55:42 -0500

    Added main Menu v0.0.1
    Created a functional menu
    
    missing design
    missing audio
    missing quality
    missing Fullscreen funcionality
    missing Resolution

---
+++ RogueGame/Assets/Scripts/MainMenuScripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void SettingsMenu()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ RogueGame/Assets/Scripts/SettingsScript/SettingsMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
