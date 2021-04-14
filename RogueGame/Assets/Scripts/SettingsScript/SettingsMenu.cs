4c596f0ceff5aab86498b44747c0b3711d3cf414
huligabuliga <jonastclausen@gmail.com>
2021-04-14 15:42:41 -0500

    resupdate done

--- RogueGame/Assets/Scripts/SettingsScript/SettingsMenu.cs
+++ RogueGame/Assets/Scripts/SettingsScript/SettingsMenu.cs
@@ -16,14 +16,28 @@ public class SettingsMenu : MonoBehaviour
         resolutionDrowpdown.ClearOptions();
 
         List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
 
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDrowpdown.AddOptions(options);
+        resolutionDrowpdown.value = currentResolutionIndex;
+        resolutionDrowpdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
     public void BackToMenu()
