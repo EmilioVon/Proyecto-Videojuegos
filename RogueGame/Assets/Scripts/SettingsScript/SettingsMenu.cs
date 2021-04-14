25e110a23c4b6617a807b3885617dfad0d610b6f
huligabuliga <jonastclausen@gmail.com>
2021-04-14 14:22:43 -0500

    Revert "Revert "main menu changed""
    This reverts commit b274bbdf4832280a978067f928c27161bf48e650.

--- RogueGame/Assets/Scripts/SettingsScript/SettingsMenu.cs
+++ RogueGame/Assets/Scripts/SettingsScript/SettingsMenu.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
@@ -11,15 +13,57 @@ public class SettingsMenu : MonoBehaviour
         SceneManager.LoadScene("menu");
     }
 
-    // Start is called before the first frame update
-    void Start()
+    //audio setting
+    public AudioMixer audioMixer;
+   public void SetVolume(float volume)
     {
-        
+        audioMixer.SetFloat("volume", volume);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    //quality setting
+    public void SetQuality (int qualityIndex)
     {
-        
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    //fullscreen setting
+    public void SetFullscreen(bool IsFullScreen)
+    {
+        Screen.fullScreen = IsFullScreen;
+    }
+
+
+    Resolution[] Resolutions;
+    public Dropdown resolutionsDropdown;
+    private void Start()
+    {
+       Resolutions=  Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0; 
+
+        for (int i = 0; i < Resolutions.Length; i++)
+        {
+            string option = Resolutions[i].width + " x " + Resolutions[i].height;
+            options.Add(option);
+
+            if (Resolutions[i].width== Screen.currentResolution.width && Resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
+    }
+
+    public void SetResoluton(int ResolutionIndex)
+    {
+        Resolution resolution = Resolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height, Screen.fullScreen);
     }
 }
