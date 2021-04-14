4fe7df2f6f78375e4b95bc6f719423c708f36954
AndresRamirez0394 <A01720068@itesm.mx>
2021-04-14 17:53:55 -0500

    GoblinPatrol

---
+++ RogueGame/Assets/Sprites/enemies/goblin/Patrol.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float vel;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform[] Waypoints;
+    private int lugarRand;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        lugarRand = Random.Range(0, Waypoints.Length);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, Waypoints[lugarRand].position, vel * Time.deltaTime);
+   
+            if(Vector2.Distance(transform.position, Waypoints[lugarRand].position) < 0.2f)
+            {
+                if(waitTime <= 0)
+                {
+                    lugarRand = Random.Range(0, Waypoints.Length);
+                    waitTime = startWaitTime;
+                } else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
+        
+}
