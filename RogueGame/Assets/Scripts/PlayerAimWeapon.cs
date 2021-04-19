b270a73d441f0ceb5e8c8d087b3b28580cc8a6f6
huligabuliga <jonastclausen@gmail.com>
2021-04-19 16:50:00 -0500

    Rotating sword with mouse

--- RogueGame/Assets/Scripts/PlayerAimWeapon.cs
+++ RogueGame/Assets/Scripts/PlayerAimWeapon.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class PlayerAimWeapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform aimTransform;
+
+    private void Awake()
     {
-        
+        aimTransform = transform.Find("Aim");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+       Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+        Debug.Log(angle);
     }
 }
