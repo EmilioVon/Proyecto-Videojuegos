1c0b8b5dc969479369c9eefb9fab6eae9fc8b866
EmilioVon <36940198+EmilioVon@users.noreply.github.com>
2021-04-21 17:13:47 -0500

    Edit Sound Effects

---
+++ RogueGame/Assets/SFX/GameSFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSFX : MonoBehaviour
+{
+public AudioSource GrassStep;
+public AudioSource DaggerWoosh;
+public AudioSource LittleMonsterLaugh;
+
+    public void PlayGrassStep() {
+    GrassStep.Play ();
+}
+    public void PlayDaggerWoosh() {
+    DaggerWoosh.Play ();
+}
+    public void PlayLittleMonsterLaugh() {
+    LittleMonsterLaugh.Play ();
+}
+
+}
---
+++ RogueGame/Assets/SFX/MENUSFX.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MENUSFX : MonoBehaviour
+{
+   public AudioSource ArcadeScoreSound;
+   public AudioSource PositiveInterface;
+   
+
+   public void PlayArcadeScoreSound() {
+       ArcadeScoreSound.Play ();
+       
+   }
+    public void PlayPositiveInterface() {
+       PositiveInterface.Play ();
+    }
+}
