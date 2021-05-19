fb7f1eb4c0c1ca045a7259aa8e37f523c7f61368
huligabuliga <jonastclausen@gmail.com>
2021-05-18 21:04:46 -0500

    healthbar

---
+++ RogueGame/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHealth = 4;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage; 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Debug.Log("damage taken");
+            TakeDamage(1);
+        }
+    }
+}
