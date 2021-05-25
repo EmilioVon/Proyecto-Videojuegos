2385bc222587823d8d830748cffdfb1879abc9f8
AndresRamirez0394 <A01720068@itesm.mx>
2021-05-24 21:17:02 -0500

    Update Script SceneTransition

--- RogueGame/Assets/Scripts/SceneTransition.cs
+++ RogueGame/Assets/Scripts/SceneTransition.cs
@@ -11,7 +11,7 @@ public class SceneTransition : MonoBehaviour
     {
         if(other.CompareTag("Player") && !other.isTrigger)
         {
-            SceneManager.LoadScene(sceneToLoad);
+            SceneManager.LoadScene("NuevoNivel");
         }
     }
 }
