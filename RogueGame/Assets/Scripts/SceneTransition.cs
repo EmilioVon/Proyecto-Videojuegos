6231fbf16f975d0490640c5c1f63dd58e0dc5689
AndresRamirez0394 <A01720068@itesm.mx>
2021-05-24 20:38:21 -0500

    Update SceneTransition

--- RogueGame/Assets/Scripts/SceneTransition.cs
+++ RogueGame/Assets/Scripts/SceneTransition.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string sceneToLoad;
 
-    // Update is called once per frame
-    void Update()
+    public void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
