d4569df55d527b0ac9f5a214d622901c0852dd65
AndresRamirez0394 <A01720068@itesm.mx>
2021-05-24 21:50:00 -0500

    Musica Continua

---
+++ RogueGame/Assets/Scripts/MusicaContinua.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicaContinua : MonoBehaviour
+{
+   private void Awake()
+    {
+        GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
+        if(musicObj.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+}
