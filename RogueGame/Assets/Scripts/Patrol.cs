948afb84b2853408a70b0e7175c6f0f0d131a3e1
Mike Tanori <miguel.tanori@hotmail.com>
2021-05-25 22:11:10 -0500

    Revert "Pathfinding y bullets"
    This reverts commit 35a0ed5c3e7087480cd07d2f222db34650b82da7, reversing
    changes made to f367fdd425b52b5f450bdb29038815fdb75114a7.

--- RogueGame/Assets/Scripts/Patrol.cs
+++ RogueGame/Assets/Scripts/Patrol.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Pathfinding;
 
 public class Patrol : MonoBehaviour
 {
@@ -10,13 +9,9 @@ public class Patrol : MonoBehaviour
     public float startWaitTime;
     public int health;
     public AudioClip BloodSplash;
-<<<<<<< HEAD
     public GameObject deathparticle;
-=======
-    public AIPath aiPath;
-    
->>>>>>> 05/25
 
+    public Transform[] Waypoints;
     private int lugarRand;
    
 
@@ -24,35 +19,39 @@ public class Patrol : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        waitTime = startWaitTime;
+        lugarRand = Random.Range(0, Waypoints.Length);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (health <= 0)
+        transform.position = Vector2.MoveTowards(transform.position, Waypoints[lugarRand].position, vel * Time.deltaTime);
+   
+            if(Vector2.Distance(transform.position, Waypoints[lugarRand].position) < 0.2f)
+            {
+                if(waitTime <= 0)
+                {
+                    lugarRand = Random.Range(0, Waypoints.Length);
+                    waitTime = startWaitTime;
+                } else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+            if(health <= 0)
         {
-<<<<<<< HEAD
         	Instantiate(deathparticle, transform.position, transform.rotation);
-=======
->>>>>>> 05/25
             Destroy(gameObject);
             AudioSource.PlayClipAtPoint(BloodSplash, transform.position);
-        }
+        
 
-        if (aiPath.desiredVelocity.x >= 0.01f)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
         }
-        else if (aiPath.desiredVelocity.x <= 0.01f)
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        }
-    }
+     }
     public void TakeDamage(int damage) {
         health -= damage;
-        //Debug.Log("Lastimaste al enemigo");
+        Debug.Log("Lastimaste al enemigo");
     }
         
 }
