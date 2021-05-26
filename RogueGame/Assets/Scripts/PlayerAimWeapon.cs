d50eca97fe696b429e58cbf2c13f1306dcfb3d9f
Mike Tanori <61033727+miketanori@users.noreply.github.com>
2021-05-25 22:00:39 -0500

    Revert "05/25"

---
+++ RogueGame/Assets/Scripts/PlayerAimWeapon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class PlayerAimWeapon : MonoBehaviour
+{
+    private Transform aimTransform;
+
+    private void Awake()
+    {
+        aimTransform = transform.Find("Aim");
+    }
+
+    private void Update()
+    {
+       Vector3 mousePosition = UtilsClass.GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
+        aimTransform.eulerAngles = new Vector3(0, 0, angle);
+        Debug.Log(angle);
+    }
+}
