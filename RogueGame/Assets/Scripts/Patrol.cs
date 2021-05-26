8c6d05a84f4f54f9387b1e61cd741124c9f3eb24
Mike Tanori <miguel.tanori@hotmail.com>
2021-05-26 00:38:32 -0500

    Patrol Pathfinding

--- RogueGame/Assets/Scripts/Patrol.cs
+++ RogueGame/Assets/Scripts/Patrol.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
+
 
 public class Patrol : MonoBehaviour
 {
@@ -10,8 +12,9 @@ public class Patrol : MonoBehaviour
     public int health;
     public AudioClip BloodSplash;
     public GameObject deathparticle;
+    public AIPath aiPath;
 
-    public Transform[] Waypoints;
+
     private int lugarRand;
    
 
@@ -19,28 +22,23 @@ public class Patrol : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        waitTime = startWaitTime;
-        lugarRand = Random.Range(0, Waypoints.Length);
+     
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, Waypoints[lugarRand].position, vel * Time.deltaTime);
-   
-            if(Vector2.Distance(transform.position, Waypoints[lugarRand].position) < 0.2f)
-            {
-                if(waitTime <= 0)
-                {
-                    lugarRand = Random.Range(0, Waypoints.Length);
-                    waitTime = startWaitTime;
-                } else
-                {
-                    waitTime -= Time.deltaTime;
-                }
-            }
-            if(health <= 0)
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (aiPath.desiredVelocity.x <= 0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+
+        if (health <= 0)
         {
         	Instantiate(deathparticle, transform.position, transform.rotation);
             Destroy(gameObject);
@@ -51,7 +49,7 @@ public class Patrol : MonoBehaviour
      }
     public void TakeDamage(int damage) {
         health -= damage;
-        Debug.Log("Lastimaste al enemigo");
+       //Debug.Log("Lastimaste al enemigo");
     }
         
 }
