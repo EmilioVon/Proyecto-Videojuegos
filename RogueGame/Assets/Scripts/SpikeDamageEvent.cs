8b2b0709d2e49ff97c6a58a5662718792f0d5e6a
huligabuliga <jonastclausen@gmail.com>
2021-06-06 20:25:27 -0500

    still working on dem spikes
    some script errors might occour still havent finished them

---
+++ RogueGame/Assets/Scripts/SpikeDamageEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDamageEvent : MonoBehaviour
+{
+
+    public bool GiveDamageFromSpikes()
+    {
+        return true;
+    }
+}
