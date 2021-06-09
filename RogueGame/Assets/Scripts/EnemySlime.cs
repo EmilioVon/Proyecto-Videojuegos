a10bb199c0fb7bba2172a20cf8bfde0118b82892
AndresRamirez0394 <A01720068@itesm.mx>
2021-06-08 19:25:36 -0500

    Update SlimeEnemy

--- RogueGame/Assets/Scripts/EnemySlime.cs
+++ RogueGame/Assets/Scripts/EnemySlime.cs
@@ -2,18 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemySlime : EnemigoVolador
+public class EnemySlime : MonoBehaviour
 {
-    
+    public Transform[] patrolPoints;
+
+    public float speed;
+    public int health;
+    public AudioClip BloodSplash;
+    public GameObject deathparticle;
+
+    Transform currentPatrolPoint;
+    int currentPatrolIndex;
+
+  
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        currentPatrolIndex = 0;
+        currentPatrolPoint = patrolPoints[currentPatrolIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+        if(Vector3.Distance (transform.position,currentPatrolPoint.position) < .1f)
+        {
+            if(currentPatrolIndex + 1 < patrolPoints.Length)
+            {
+                currentPatrolIndex++;
+            } else
+            {
+                currentPatrolIndex = 0;
+            }
+            currentPatrolPoint = patrolPoints[currentPatrolIndex];
+        }
+        if (health <= 0)
+        {
+            Instantiate(deathparticle, transform.position, transform.rotation);
+            Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(BloodSplash, transform.position);
+
+
+        }
+
     }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        //Debug.Log("Lastimaste al enemigo");
+    }
+
 }
