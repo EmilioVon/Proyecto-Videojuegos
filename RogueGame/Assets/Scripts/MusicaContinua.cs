d09a055b7177dd28331acc4effe4f2e7d93af306
AndresRamirez0394 <A01720068@itesm.mx>
2021-06-09 12:42:40 -0500

    BossMusic in Boss1

--- RogueGame/Assets/Scripts/MusicaContinua.cs
+++ RogueGame/Assets/Scripts/MusicaContinua.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MusicaContinua : MonoBehaviour
 {
    private void Awake()
     {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
-        if(musicObj.Length > 1)
+        if(musicObj.Length > 1 || sceneName == "Boss1")
         {
             Destroy(this.gameObject);
         }
