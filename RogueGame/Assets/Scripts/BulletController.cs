5739a0d300f530a54b744a159765341ee7b7dae1
Mike Tanori <miguel.tanori@hotmail.com>
2021-06-08 19:52:20 -0500

    Poder de balas

---
+++ RogueGame/Assets/Scripts/BulletController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float lifeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DeathDelay()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            collision.gameObject.GetComponent<Patrol>().TakeDamage(1);
+            Destroy(gameObject);
+        }
+    }
+}
