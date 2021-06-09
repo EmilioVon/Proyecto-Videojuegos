ebbd59c5117581ec3024ce382c829c1f19b41758
huligabuliga <jonastclausen@gmail.com>
2021-06-08 20:17:58 -0500

    Revert "Revert "slime working""
    This reverts commit 2e72fea6ec0401a99cf97352d1ad1a81789a393a.

--- RogueGame/Assets/Scripts/EnemySlime.cs
+++ RogueGame/Assets/Scripts/EnemySlime.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class EnemySlime : MonoBehaviour
 {
+    // for the patrol
     public Transform[] patrolPoints;
-
     public float speed;
+    private int randomSpot;
+    private float waitTime;
+    public float startWaitTime;
+    
+
 
     Transform currentPatrolPoint;
     int currentPatrolIndex;
 
-  
+
     Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
+        waitTime = startWaitTime; 
+        randomSpot = Random.Range(0, patrolPoints.Length);
         anim = GetComponent<Animator>();
         currentPatrolIndex = 0;
         currentPatrolPoint = patrolPoints[currentPatrolIndex];
@@ -27,6 +34,22 @@ public class EnemySlime : MonoBehaviour
     {
         // transform.Translate(Vector3.up * Time.deltaTime * speed);
 
+        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, patrolPoints[randomSpot].position) < 0.2f)
+        {   
+            if(waitTime <=0)
+            {
+                randomSpot = Random.Range(0, patrolPoints.Length);
+                waitTime = startWaitTime; 
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }                
+        }
+
+        /*
         if(Vector3.Distance (transform.position,currentPatrolPoint.position) < .1f)
         {
             if(currentPatrolIndex + 1 < patrolPoints.Length)
@@ -38,7 +61,8 @@ public class EnemySlime : MonoBehaviour
             }
             currentPatrolPoint = patrolPoints[currentPatrolIndex];
         }
-     
+        */
+
 
     }
   
